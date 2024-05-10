80d747fca77ee82b8e982c512a7b89336ba12313
RaitonOwO <jhoanowo@gmail.com>
2024-05-10 17:14:58 -0500

    se creo el productos_tienda.json

--- menus.py
+++ menus.py
@@ -14,7 +14,8 @@ def menu_planes():
     print("2.Para agregar plan prepago")
     print("3.Para agregar plan de fibra optica")
     print("4.Para modificar un plan")
-    print("5.Para salir")
+    print("5.eliminar servicio")
+    print("6.Para salir")
 
 def menu_historiales():
     print("1.Para mostar el historial de compras")
